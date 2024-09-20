f072b0d32a435970c4c47103e48ff371addd39ef
Limitless Playing <limitlessplaying@gmail.com>
2024-09-20 09:42:24 +0545

    Encoding and decoing URL

---
+++ UrlEncoderDecoder.java
@@ -0,0 +1,24 @@
+package np;
+
+import java.net.*;
+import java.io.*;
+
+public class UrlEncoderDecoder {
+	public static void main(String[] args) {
+		String url = "https:www.example.com";
+		try {
+			System.out.println("Url before encoding: "+url);
+			//Encoding the URL
+			String encoded_url = URLEncoder.encode(url, "utf-8");
+			System.out.println("Url after encoding: "+encoded_url);
+			
+			//Decoding the URL
+			String decoded_url = URLDecoder.decode(encoded_url, "utf-8");
+			System.out.println("Url after decoding: "+decoded_url);
+		}catch(UnsupportedEncodingException e) {
+			System.out.println(e.getMessage());
+		}
+
+		
+	}
+}
