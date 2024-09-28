26150eae4b5c7920c846f7925bb7a96546e97a0f
Suman Regmi <limitlessplaying@gmail.com>
2024-09-28 21:47:18 +0545

    Reading from server using socket

--- SocketServer.java
+++ SocketServer.java
@@ -11,22 +11,18 @@ public class SocketServer {
         int port = 80;
         try {
             Socket socket = new Socket(host, port);
-            
             // Sending request to the server
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            
             out.println("GET / HTTP/1.1");
             out.println("Host: " + host);
             out.println("Connection: close");
             out.println();  // Blank line indicates end of request headers
-            
             // Reading response from the server
             String line;
             while ((line = in.readLine()) != null) {
                 System.out.println(line);
             }
-
             socket.close();
         } catch (Exception e) {
             e.printStackTrace();
